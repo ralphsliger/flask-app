6d45d6d901ad00241dc6a2453bc1d0ef0ee10351
Ralph <ralphsliger1@gmail.com>
2020-03-07 13:33:17 -0500

    commit

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from datetime import datetime
 from flaskext.mysql import MySQL
+import redis
+
+
 
 
 app = Flask(__name__)
 
+db = redis.Redis('localhost') # reddis
+
+
+##docker
+def getMysqlConnection():
+    return mysql.connector.connect(user='root', host='mysql', port='3306', password='secret', database='flaskapp')
+
+
 # Mysql Connection
 app.config['MYSQL_HOST'] = 'localhost' 
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Supreme22'
+app.config['MYSQL_PASSWORD'] = 'secret
 app.config['MYSQL_DB'] = 'flaskapp'
 mysql = MySQL(app)
 
 # settings
 app.secret_key = "mysecretkey"
 
+
 # routes
+
 @app.route('/')
 def Index():
     cursor = mysql.get_db().cursor()
@@ -23,16 +37,23 @@ def Index():
     cur.close()
     return render_template('index.html', data = data)
 
+
 @app.route('/', methods=['POST'])
 def add():
     if request.method == 'POST':
         temperature = request.form['temperature']
-        date = request.form['date']
+        date = datetime.strptime(request.form['date'],'%Y-%m-%d %H:%M:%S').strftime('%Y%m%d%H%M%S')
+        time  = datetime.now().strftime('%Y%m%d%H%M%S')
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO data (temperature, date) VALUES (%s,%s)", (temperature, date))
+        cur.execute("INSERT INTO data (temperature, date) VALUES (%s,%s,%s)", (temperature, date, time))
         mysql.connection.commit()
         flash('Added successfully')
         return redirect(url_for('Index'))
+
+
+@app.route('/index', methods=['GET'])
+def show_ar():
+    return redirect(url_for('arquitectura'))
 
 
 if __name__ == '__main__':
